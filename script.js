1e085884ac1f25c371a88a2b242969f045638de9
aubarkas3 <adbarkas@gmail.com>
2024-09-29 22:25:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function buyTicket(platform) {
     alert('Redirecting to ' + platform + '...');
 }
+function handleSearch(event) {
+    event.preventDefault(); // Prevents the form from submitting
+    alert('Searching for your event');
+}
