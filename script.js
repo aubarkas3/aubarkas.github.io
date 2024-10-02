cec43a96b7a0c71d85e8da8efd2f9e262f88f396
aubarkas3 <adbarkas@gmail.com>
2024-10-02 15:52:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function buyTicket(platform) {
-    alert('Redirecting to ' + platform + '...');
-}
+
 function handleSearch(event) {
     event.preventDefault(); // Prevents the form from actually submitting
     alert('Looking for your event'); // Displays the alert message
