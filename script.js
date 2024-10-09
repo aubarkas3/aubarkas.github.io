6166eccc8a3fe94577405111a0181b0a2ce9f06d
aubarkas3 <adbarkas@gmail.com>
2024-10-08 21:48:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,16 @@ document.querySelectorAll('.button').forEach(button => {
 
 function toggleRows() {
     const rows = document.querySelectorAll('#myTable tbody tr');
-    const viewMoreButton = document.getElementById('viewMore');
-    // Check if the fifth row is visible
+    const viewMore = document.getElementById('viewMore');
     if (rows[4].style.display === 'none' || rows[4].style.display === '') {
-        // Make all rows visible
-        rows.forEach(row => {
-            row.style.display = 'table-row';
-        });
-        viewMoreButton.textContent = 'View Less';
+        for (let i = 4; i < rows.length; i++) {
+            rows[i].style.display = 'table-row';
+        }
+        viewMore.textContent = 'View Less..';
     } else {
-        // Hide all rows except the first four
         for (let i = 4; i < rows.length; i++) {
             rows[i].style.display = 'none';
         }
-        viewMoreButton.textContent = 'View More';
+        viewMore.textContent = 'View More..';
     }
 }
